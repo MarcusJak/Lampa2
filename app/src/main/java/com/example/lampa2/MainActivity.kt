22181d4325e5f9eb6b78939dadb3d9943198e6c8
Oozna <johanssonericwilliam@gmail.com>
2021-03-29 14:22:56 +0200

    Fixade era knappar och gjort så att RGB knappen länkar till en ny sida

--- app/src/main/java/com/example/lampa2/MainActivity.kt
+++ app/src/main/java/com/example/lampa2/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.lampa2
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rgbButton = findViewById<Button>(R.id.rgbButton)
+        rgbButton.setOnClickListener {
+            val intent = Intent(this,  RGB::class.java)
+            startActivity(intent)
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/lampa2/RGB.kt
@@ -0,0 +1,11 @@
+package com.example.lampa2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RGB : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_r_g_b)
+    }
+}
