6254f375a32cd30aa21f6d5afe9e3aba5c72c98e
Oozna <johanssonericwilliam@gmail.com>
2021-03-29 14:48:54 +0200

    Lade till möjlighet att backa tillbaka frpn RGB sidan

--- app/src/main/java/com/example/lampa2/RGB.kt
+++ app/src/main/java/com/example/lampa2/RGB.kt
@@ -7,5 +7,9 @@ class RGB : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_r_g_b)
+
+        val actionBar = supportActionBar
+        actionBar!!.title = "RGB Picker"
+        actionBar.setDisplayHomeAsUpEnabled(true)
     }
 }
