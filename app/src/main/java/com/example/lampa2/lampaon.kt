011169a7a21934eb74a9161344a8f080c97b76b7
MarcusJak <78412619+MarcusJak@users.noreply.github.com>
2021-03-30 15:49:21 +0200

    fixa ljus styrka seekbar

--- app/src/main/java/com/example/lampa2/lampaon.kt
+++ app/src/main/java/com/example/lampa2/lampaon.kt
@@ -5,6 +5,10 @@ import android.net.MailTo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.SeekBar
+import android.widget.TextView
+import android.widget.Toast
+
 
 class lampaon : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,5 +24,31 @@ class lampaon : AppCompatActivity() {
             val intent = Intent(this,  MainActivity::class.java)
             startActivity(intent)
         }
+
+        val styrka = findViewById<SeekBar>(R.id.styrka)
+        val textView2 = findViewById<TextView>(R.id.textView2)
+        var startPoint=0
+        var endPoint=0
+
+        styrka.setOnSeekBarChangeListener(object :  SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                textView2.text=progress.toString()
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                if (seekBar != null) {
+                    startPoint=seekBar.progress
+                }
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                if (seekBar != null) {
+                    endPoint=seekBar.progress
+                }
+                Toast.makeText(this@lampaon,"ändring  ${endPoint-startPoint}%", Toast.LENGTH_SHORT).show()
+
+            }
+
+        })
     }
 }
