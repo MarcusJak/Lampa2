58c8caa399f0575cbe9638bebaf30b2bf246cfbd
Oozna <johanssonericwilliam@gmail.com>
2021-04-12 20:01:16 +0200

    Fixade lite style på RGB-sidan

--- app/src/main/java/com/example/lampa2/RGB.kt
+++ app/src/main/java/com/example/lampa2/RGB.kt
@@ -19,43 +19,65 @@ class RGB : AppCompatActivity(){
         var red = 0
         var green = 0
         var blue = 0
+        val hexV = findViewById<TextView>(R.id.rgbHexValue)
+        val rgbV = findViewById<TextView>(R.id.rgbValue)
         val view = findViewById<View>(R.id.rgbView)
         val sbR = findViewById<SeekBar>(R.id.redBar)
         val sbG = findViewById<SeekBar>(R.id.greenBar)
         val sbB = findViewById<SeekBar>(R.id.blueBar)
-        val rT = findViewById<TextView>(R.id.redNumber)
-        val gT = findViewById<TextView>(R.id.greenNumber)
-        val bT = findViewById<TextView>(R.id.blueNumber)
 
+
+
+        fun toHex(red : Int , green : Int, blue : Int) : String{
+            var code = "#"
+            code += java.lang.Integer.toHexString(red)
+            code += java.lang.Integer.toHexString(green)
+            code += java.lang.Integer.toHexString(blue)
+            return code
+        }
+        
+        fun toRgb(red : Int , green : Int, blue : Int) : String{
+            var code = "RGB ("
+            val delim =  ", "
+            code += red.toString()
+            code += delim
+            code += green.toString()
+            code += delim
+            code += blue.toString()
+            code += ")"
+            return code
+        }
 
         sbR.setOnSeekBarChangeListener(object :  SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                rT.text = progress.toString()
                 red = progress
                 view.setBackgroundColor(Color.rgb(red, green, blue))
+                hexV.text = toHex(red, green, blue)
+                rgbV.text = toRgb(red, green, blue)
             }
             override fun onStartTrackingTouch(seekBar: SeekBar?){}
             override fun onStopTrackingTouch(seekBar: SeekBar?) {}
         })
         sbG.setOnSeekBarChangeListener(object :  SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                gT.text = progress.toString()
                 green = progress
                 view.setBackgroundColor(Color.rgb(red, green, blue))
+                hexV.text = toHex(red, green, blue)
+                rgbV.text = toRgb(red, green, blue)
             }
             override fun onStartTrackingTouch(seekBar: SeekBar?){}
             override fun onStopTrackingTouch(seekBar: SeekBar?) {}
         })
         sbB.setOnSeekBarChangeListener(object :  SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                bT.text = progress.toString()
                 blue = progress
                 view.setBackgroundColor(Color.rgb(red, green, blue))
+                hexV.text = toHex(red, green, blue)
+                rgbV.text = toRgb(red, green, blue)
             }
             override fun onStartTrackingTouch(seekBar: SeekBar?){}
             override fun onStopTrackingTouch(seekBar: SeekBar?) {}
         })
-
     }
 
 }
