88343c3675fdd875de343a44103796ffa23d9c87
Mattias <matte.wahlstrom10@gmail.com>
2021-04-22 13:05:51 +0200

    nattläge för appen (borde funka)

--- app/src/main/java/com/example/lampa2/settings.kt
+++ app/src/main/java/com/example/lampa2/settings.kt
@@ -1,8 +1,10 @@
 package com.example.lampa2
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Switch
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import android.widget.Button
 
 class settings : AppCompatActivity() {
@@ -10,17 +12,22 @@ class settings : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
-        val btnnightmode = findViewById<Button>(R.id.btnnightmode)
+        // Declare the switch from the layout file
+        val btn = findViewById<Switch>(R.id.switch1)
 
-        btnnightmode.setOnClickListener {
+        // set the switch to listen on checked change
+        btn.setOnCheckedChangeListener { _, isChecked ->
+
+            // if the button is checked, i.e., towards the right or enabled
+            // enable dark mode, change the text to disable dark mode
+            // else keep the switch text to enable dark mode
+            if (btn.isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                btn.text = "Disable dark mode"
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                btn.text = "Enable dark mode"
+            }
         }
-
-        val btnpresets = findViewById<Button>(R.id.btnpresets)
-
-        btnpresets.setOnClickListener {
-            val intent = Intent(this,  presets::class.java)
-            startActivity(intent)
-        }
-
     }
 }
