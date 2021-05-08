75aead827a45816c5ed5c434ff3161e95555c581
Mattias <matte.wahlstrom10@gmail.com>
2021-05-08 10:34:35 +0200

    La till layout för timer

--- app/src/main/java/com/example/lampa2/settings.kt
+++ app/src/main/java/com/example/lampa2/settings.kt
@@ -1,16 +1,34 @@
 package com.example.lampa2
 
+import android.app.Activity
+import android.content.Context
 import android.content.Intent
+import android.content.res.Configuration
 import android.os.Bundle
 import android.widget.Switch
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import android.widget.Button
+import androidx.appcompat.app.AlertDialog
+import java.util.*
 
 class settings : AppCompatActivity() {
+
+    lateinit var btn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        val actionBar = supportActionBar
+        actionBar!!.title = resources.getString(R.string.app_name)
+
+        btn = findViewById(R.id.btnlng)
+        btn.setOnClickListener {
+
+            showchangelang()
+
+        }
 
         //sätt på UV
         val btnpreset = findViewById<Button>(R.id.btnpreset)
@@ -38,5 +56,47 @@ class settings : AppCompatActivity() {
             }
         }
     }
+
+    private fun showchangelang() {
+        val listItems = arrayOf("Svenska", "English")
+
+        val mBuilder = AlertDialog.Builder(this@settings)
+        mBuilder.setTitle("Chose Language")
+        mBuilder.setSingleChoiceItems(listItems, -1) { dialog, which ->
+            if (which == 0) {
+                setLocate("sv-rSE")
+                recreate()
+            } else if (which == 1) {
+                setLocate("en")
+                recreate()
+            }
+            dialog.dismiss()
+        }
+        val mDialog = mBuilder.create()
+
+        mDialog.show()
+    }
+
+
+    private fun setLocate(Lang: String) {
+        val locale = Locale(Lang)
+        Locale.setDefault(locale)
+        val config = Configuration()
+        config.locale = locale
+        baseContext.resources.updateConfiguration(config, baseContext.resources.displayMetrics)
+
+        val editor = getSharedPreferences("Settings", Context.MODE_PRIVATE).edit()
+        editor.putString("My_Lang", Lang)
+        editor.apply()
+
+    }
+
+    private fun loadLocate() {
+        val sharedPreferences = getSharedPreferences("Settings", Activity.MODE_PRIVATE)
+        val language = sharedPreferences.getString("My_Lang", "")
+        setLocate(language.toString())
+    }
 }
+
+
 
