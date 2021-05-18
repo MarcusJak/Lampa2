6babdf1167f7e857d2d9ce225d90854c372b0574
Oozna <johanssonericwilliam@gmail.com>
2021-05-18 11:32:48 +0200

    Select Device Addded

--- app/src/main/java/com/example/lampa2/MainActivity.kt
+++ app/src/main/java/com/example/lampa2/MainActivity.kt
@@ -12,18 +12,20 @@ import kotlinx.android.synthetic.main.activity_main.*
 import java.io.IOException
 import java.util.*
 
+lateinit var btSocket : BluetoothSocket
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var mBluetoothAdapter: BluetoothAdapter
-    lateinit var btSocket : BluetoothSocket
-
+    lateinit var macstring : String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+        macstring = intent.getStringExtra("MAC-Address").toString()
         connect()
 
         rgbButton.setOnClickListener {
@@ -53,13 +55,11 @@ class MainActivity : AppCompatActivity() {
 
     }
     fun connect() {
-
-        val device = mBluetoothAdapter.getRemoteDevice("30:14:08:26:16:18")
+        val device = mBluetoothAdapter.getRemoteDevice(macstring)
         Log.d("", "Connecting to ... $device")
         mBluetoothAdapter.cancelDiscovery()
         try {
             btSocket = device.createRfcommSocketToServiceRecord(UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"))
-            /* Here is the part the connection is made, by asking the device to create a RfcommSocket (Unsecure socket I guess), It map a port for us or something like that */
             btSocket.connect()
         } catch (e: IOException) {
             try {
@@ -68,30 +68,14 @@ class MainActivity : AppCompatActivity() {
                 Log.d("", "Unable to end the connection")
                 Toast.makeText(applicationContext, "Unable to end the connection", Toast.LENGTH_SHORT).show()
             }
-
             Log.d("", "Socket creation failed")
             Toast.makeText(applicationContext, "Socket creation failed", Toast.LENGTH_SHORT).show()
         }
-
-        //beginListenForData()
-        /* this is a method used to read what the Arduino says for example when you write Serial.print("Hello world.") in your Arduino code */
     }
 
     private fun writeData(data: String) {
-        var outStream = btSocket.outputStream
-        try {
-            outStream = btSocket.outputStream
-        } catch (e: IOException) {
-            //Log.d(FragmentActivity.TAG, "Bug BEFORE Sending stuff", e)
-        }
+        val outStream = btSocket.outputStream
         val msgBuffer = data.toByteArray()
-
-        try {
-            outStream.write(msgBuffer)
-        } catch (e: IOException) {
-            //Log.d(FragmentActivity.TAG, "Bug while sending stuff", e)
-        }
-
+        outStream.write(msgBuffer)
     }
-
 }
